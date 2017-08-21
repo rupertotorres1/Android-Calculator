94d5a5c1bf04a465429619bdf4fc5e2cfe6c927c
Andres R. Torres <rupertotorres1@gmail.com>
2017-08-21 17:35:19 -0500

    basic functionality, still have to account for special cases

--- Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
@@ -4,15 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
     private final static String TAG = CalculatorActivity.class.getSimpleName();
-
+    TextView result;
+    TextView input1;
+    TextView input2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        result = (TextView) findViewById(R.id.textResult);
+        input1 = (TextView) findViewById(R.id.firstInput);
+        input2 = (TextView) findViewById(R.id.secondInput);
+
     }
 
     /**
@@ -23,29 +30,33 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
      */
     @Override
     public void onClick(View v) {
+        double output = 0.0;
+        double num1 = Double.parseDouble(input1.getText().toString());
+        double num2 = Double.parseDouble(input2.getText().toString());
+        //TODO: fix errors (division by zero and input not set
         switch (v.getId())
         {
             case R.id.operation_add:
             {
-                //TODO add function
+                output = num1 + num2;
                 break;
             }
             case R.id.operation_sub:
             {
-                //TODO subtract function
+                output = num1 - num2;
                 break;
             }
             case R.id.operation_multi:
             {
-                //TODO multiply function
+                output = num1 * num2;
                 break;
             }
             case R.id.operation_div:
             {
-                //TODO divide function
+                output = num1 / num2;
                 break;
             }
-            //TODO any extra implmentations (optional)
+            //TODO any extra implementations (optional)
             default:
             {
                 Toast.makeText(this, "Click not implmented yet", Toast.LENGTH_LONG).show();
@@ -53,5 +64,7 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
                 break;
             }
         }
+
+        result.setText("Result: " + output);
     }
 }
